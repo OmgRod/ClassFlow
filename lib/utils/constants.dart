2e9661114a6c779b32f6d5c08693708f90f872b9
OmgRod <89850217+OmgRod@users.noreply.github.com>
2025-11-29 10:18:09 +0000

    bump version

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppConstants {
   // App info
   static const String appName = 'ClassFlow';
-  static const String appVersion = '0.1.0';
+  static const String appVersion = '0.1.1';
 
   // Days of week
   static const List<String> daysOfWeek = [
